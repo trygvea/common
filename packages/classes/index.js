a44c06e6b5a7afe8535a3f00c91c8c9684d72f51
Benjamin Akar <benjaminakar2001@gmail.com>
2022-07-20 16:00:49 +0200

    docs: change package descriptions

--- packages/classes/index.js
+++ packages/classes/index.js
@@ -1,27 +1,7 @@
 const ReadFile = require('./src/read-file');
 const EikConfig = require('./src/eik-config');
-const FileMapping = require('./src/file-mapping');
-const LocalFileLocation = require('./src/local-file-location');
-const RemoteFileLocation = require('./src/remote-file-location');
-const ResolvedFiles = require('./src/resolved-files');
-const MissingConfigError = require('./src/missing-config-error');
-const MultipleConfigSourcesError = require('./src/multiple-config-sources-error');
-const InvalidConfigError = require('./src/invalid-config-error');
-const SingleDestMultipleSourcesError = require('./src/single-dest-multiple-source-error');
-const NoFilesMatchedError = require('./src/no-files-matched-error');
-const CustomError = require('./src/custom-error');
 
 module.exports = {
     ReadFile,
     EikConfig,
-    FileMapping,
-    LocalFileLocation,
-    RemoteFileLocation,
-    ResolvedFiles,
-    MissingConfigError,
-    MultipleConfigSourcesError,
-    InvalidConfigError,
-    SingleDestMultipleSourcesError,
-    NoFilesMatchedError,
-    CustomError,
 };
--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -1,9 +1,5 @@
-// const localAssets = require('./src/local-assets');
-// const getDefaults = require('./src/get-defaults');
-// const configStore = require('./src/config-store');
 const typeSlug = require('./src/type-slug');
 const typeTitle = require('./src/type-title');
-// const resolveFiles = require('./src/resolve-files');
 const { isStream, isReadableStream } = require('./src/stream');
 const {
     addTrailingSlash,
@@ -19,10 +15,6 @@ module.exports = {
     removeLeadingSlash,
     isStream,
     isReadableStream,
-    // localAssets,
-    // getDefaults,
-    // resolveFiles,
-    // configStore,
     typeSlug,
     typeTitle,
 };
