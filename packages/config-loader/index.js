b3e233f3a4e8ef40e816bd38389c303bc5d1b47e
Benjamin Akar <benjaminakar2001@gmail.com>
2022-07-26 10:53:28 +0300

    refactor: move local-assets into root of config-loader and publicly expose it

--- packages/config-loader/index.js
+++ packages/config-loader/index.js
@@ -2,10 +2,12 @@ const ReadFile = require('./src/classes/read-file.js');
 const EikConfig = require('./src/eik-config.js');
 const getDefaults = require('./src/get-defaults.js');
 const configStore = require('./src/config-store.js');
+const localAssets = require('./src/local-assets');
 
 module.exports = {
     ReadFile,
     EikConfig,
     getDefaults,
     configStore,
+    localAssets,
 };
